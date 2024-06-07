67b8817cef1558a9bb78be403ce0b48791053701
GustavoHerreroNunes <ghnunes2010@gmail.com>
2024-06-06 21:08:04 -0300

    Add: firebase-related files for credentials validation

---
+++ firebase-files/firebase-init.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+      apiKey: "AIzaSyCufe1GouREmZWjSvcqlijM4MdHXBb_bQQ",
+      authDomain: "auth-login-register-44872.firebaseapp.com",
+      projectId: "auth-login-register-44872",
+      storageBucket: "auth-login-register-44872.appspot.com",
+      messagingSenderId: "440945764365",
+      appId: "1:440945764365:web:b2d51a2e1821e3c668cd44"
+   };
+   firebase.initializeApp(firebaseConfig)
