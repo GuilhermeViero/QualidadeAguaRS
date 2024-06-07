004b104011418525fbca7f0b955274b07b28b92f
Gustavo Herrero Nunes <51753426+GustavoHerreroNunes@users.noreply.github.com>
2024-06-07 01:40:05 +0000

    Change: Showing only most recent indicators within the popup

--- QualidadeAgua/pages/mapManager/script.js
+++ QualidadeAgua/pages/mapManager/script.js
@@ -63,22 +63,23 @@ function initMapViewInfo() {
     data.forEach(function (item) {
         var marker = L.marker([item.latitude, item.longitude]).addTo(map);
         markers.push(marker); // Adiciona o marcador ao array de marcadores
-        var popupContent = item.data.map(d => `
-            <b>Data dos testes:</b> ${d.data}<br>
-            <b>Balneabilidade:</b> ${qualityCalculator.calcBalneability(d.coliformes)}<br>
-            <b>IQA:</b> ${qualityCalculator.calcIQA(d.iqa)}<br>
-            <b>pH:</b> ${d.ph}<br>
-            <b>Oxigênio Dissolvido (OD):</b> ${d.od} mg/L<br>
-            <b>Demanda Bioquímica de Oxigênio (DBO):</b> ${d.dbo} mg/L<br>
-            <b>Nitrogênio Total:</b> ${d.nitrogenio} mg/L<br>
-            <b>Fósforo Total:</b> ${d.fosforo} mg/L<br>
-            <b>Metais Pesados:</b> ${d.metais}<br>
-            <b>Sólidos Suspensos Totais (SST):</b> ${d.sst} mg/L<br>
-            <b>Coliformes Fecais e E. Coli:</b> ${d.coliformes} UFC/100 mL<br>
-            <b>Salinidade:</b> ${d.salinidade} ‰<br>
-            <b>Bairro:</b> ${d.bairro}<br>
-            <b>Cidade:</b> ${d.cidade}
-        `).join('<br><hr><br>');
+        const mostRecentData = item.data[item.data.length - 1];
+        var popupContent = `
+            <b>Data dos testes:</b> ${mostRecentData.data}<br>
+            <b>Balneabilidade:</b> ${qualityCalculator.calcBalneability(mostRecentData.coliformes)}<br>
+            <b>IQA:</b> ${qualityCalculator.calcIQA(mostRecentData.iqa)}<br>
+            <b>pH:</b> ${mostRecentData.ph}<br>
+            <b>Oxigênio Dissolvido (OD):</b> ${mostRecentData.od} mg/L<br>
+            <b>Demanda Bioquímica de Oxigênio (DBO):</b> ${mostRecentData.dbo} mg/L<br>
+            <b>Nitrogênio Total:</b> ${mostRecentData.nitrogenio} mg/L<br>
+            <b>Fósforo Total:</b> ${mostRecentData.fosforo} mg/L<br>
+            <b>Metais Pesados:</b> ${mostRecentData.metais}<br>
+            <b>Sólidos Suspensos Totais (SST):</b> ${mostRecentData.sst} mg/L<br>
+            <b>Coliformes Fecais e E. Coli:</b> ${mostRecentData.coliformes} UFC/100 mL<br>
+            <b>Salinidade:</b> ${mostRecentData.salinidade} ‰<br>
+            <b>Bairro:</b> ${mostRecentData.bairro}<br>
+            <b>Cidade:</b> ${mostRecentData.cidade}
+        `;
         marker.bindPopup(popupContent);
     });
 }
