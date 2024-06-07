cf30a3b4c86beb07363c2fa519dc5284f499d34a
Gustavo Herrero Nunes <51753426+GustavoHerreroNunes@users.noreply.github.com>
2024-06-07 01:35:30 +0000

    Add: private routes and elements

--- QualidadeAgua/auth-guard.js
+++ QualidadeAgua/auth-guard.js
@@ -1,5 +1,28 @@
 firebase.auth().onAuthStateChanged(user =>{
-    if(!user){
-        window.location.href = "../../login.html"
+    const privateElements = document.querySelectorAll('.private');
+    const publicElements = document.querySelectorAll('.public');
+    const isRoutePrivate = document.querySelector('#private_route') !== null;
+
+    if(privateElements.length){
+        privateElements.forEach((element) => {
+            element.style.display = user ? "block" : "none";
+        });
+        publicElements.forEach((element) => {
+            element.style.display = user ? "none" : "block";
+        });
+
+        document.querySelector('.logout').addEventListener('click', () => {
+            firebase.auth().signOut().then(() => {
+                console.log('Deslogou');
+                window.location.href = isRoutePrivate ? "../../index.html" : "index.html";
+            }).catch((error) => {
+                console.error(error);
+            });
+        });
+    }else if(isRoutePrivate && !user){
+        window.location.href = "pages/credentialManager/login.html"
+    }
+    else if(user){
+        window.location.href = "index.html"
     }
 })
